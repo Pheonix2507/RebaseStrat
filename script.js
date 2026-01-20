13ff1aa4932567aa6e6717335ef9ee610247a3d7
Pheonix2507 <chintub2507@gmail.com>
2026-01-20 20:56:22 +0530

    Commit no 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('randomBtn').addEventListener('click', function() {
         'You clicked the button!',
         'Random message: Have a great day!',
         'This is a base project.',
-        'Keep coding!'
+        'Keep coding further editing the code to check and merges!'
     ];
     const randomIndex = Math.floor(Math.random() * messages.length);
     document.getElementById('message').textContent = messages[randomIndex];
