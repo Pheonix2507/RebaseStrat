f07db9bec74f5cbd3afbb402fa971306ad09be9d
Ghost <chintub2507@gmail.com>
2026-01-20 21:17:28 +0530

    Checking for the issue (#2)
    * Checking for the issue
    
    * Flascking back
    
    * Lets see

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('randomBtn').addEventListener('click', function() {
     const messages = [
         'Changing texts to check!',
-        'Checking for the commits in this new branch as well!',
+        'wil it fail!',
         'Random message lets see for this great day!',
         'This is a base project.',
-        'Keep coding further editing the code to check and merges!'
+        'changing the text to check more!'
     ];
     const randomIndex = Math.floor(Math.random() * messages.length);
     document.getElementById('message').textContent = messages[randomIndex];
